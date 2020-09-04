49336984e1ba38a8cd00ff6763948aedbf720356
Jasper Kent <jasper_kent@hotmail.com>
2020-09-04 08:43:37 +0100

    Add project files.

---
+++ CollatzCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollatzCSharp
+{
+    class Program
+    {
+        public static IEnumerable<(uint count, uint value)> Collatz(uint seed)
+        {
+            uint count = 1;
+
+            yield return (count, seed);
+
+            while (seed > 1)
+            {
+                if (seed % 2 == 0)
+                    seed /= 2;
+                else
+                    seed = 3 * seed + 1;
+
+                yield return (++count, seed);
+            }
+        }
+
+        static void Main()
+        {
+            foreach (var (count, value) in Collatz(1000))
+            {
+                Console.WriteLine($"{count}: {value}.");
+            }
+        }
+    }
+}
